c0e47c4fea82041b41d8aea5b6f91c75b0d65ad5
Tanvipatel23 <113066855+Tanvipatel23@users.noreply.github.com>
2022-11-07 23:52:04 -0600

    updated todo_test.js

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -8,7 +8,6 @@ describe('todo test suite', () => {
     });
     
     let todo_service = new todoservice();
-
     test("if service instance is created", () => {
         expect(todo_service instanceof todoservice).toBe(true);
     });
@@ -22,6 +21,34 @@ describe('todo test suite', () => {
     
 
     // Write all your test cases here that corresponds to software requirements
+  
 
+    var id = 1;
+
+    var new_id = 3;
+
+
+    test("add_todos", () => {
+        expect(todo_service.add_todo({
+            tittle: "task4",
+            description: "description-4",
+            done: false
+        }).todo.length).toEqual(4);
+    });
+
+    test("delete_todos", () => {
+        expect(todo_service.delete_todo(id).todo.length).toEqual(2);
+    });
+    test("update_todo", () => {
+        expect(todo_service.update_todo(new_id,
+            {tittle: "task5",
+             description: "bring milk from shop",
+             done: false})
+             .todo[3]).toEqual({
+            tittle: "task5",
+            description: "bring milk from shop",
+            done: false
+        });
+      });
 
 });
